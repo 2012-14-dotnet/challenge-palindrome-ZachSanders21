49eb890d4b0c61abc03ff8c7f4a7b27fd0e578c5
zach sanders <zach.sanders@revature.net>
2021-01-04 11:12:13 -0500

    removing whitespace final

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Palindrome.Domain;
 
 namespace Palindrome.Client
 {
@@ -16,7 +17,7 @@ namespace Palindrome.Client
         static void StringInput()
         {
             var input = Console.ReadLine();
-            NoSpacing(input);
+            Whitespace.NoWhiteSpace(input);
         }
     }
 }
--- Palindrome.Domain/NoSpacing.cs
+++ Palindrome.Domain/NoSpacing.cs
@@ -1,16 +1,17 @@
 using System.Linq;
+using System;
 namespace Palindrome.Domain
 {
     public class Whitespace
     {
-        public string NoWhiteSpace(string input)
-        {
-            string nospace = string.Concat(input.Where(c => !char.IsWhiteSpace(c)));
-            return nospace;
+        public static string NoWhiteSpace(string input)
+        {   
+            // string nospace = String.Concat(input.Where(c => !char.IsWhiteSpace(c)));
+            // return nospace;
             
-            // return new string(input.ToCharArray()
-            //     .Where(c => !char.IsWhiteSpace(c))
-            //     .ToArray());
+            return new string(input.ToCharArray()
+                .Where(c => !char.IsWhiteSpace(c))
+                .ToArray());
         }
     }
 }
